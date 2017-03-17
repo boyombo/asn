172b2cc5a80f78ff75145c3e2ace4c3e419a3a56
bayo <bayokrapht@gmail.com>
2017-03-17 08:33:10 +0100

    validation of email

--- member/forms.py
+++ member/forms.py
@@ -7,3 +7,13 @@ class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
         fields = ['email']
+
+    def clean_email(self):
+        if 'email' in self.cleaned_data:
+            try:
+                Member.objects.get(email=self.cleaned_data['email'])
+            except Member.DoesNotExist:
+                return self.cleaned_data['email']
+            else:
+                raise forms.ValidationError(
+                    'This email has been registered already')
